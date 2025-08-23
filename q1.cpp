606f71ecabf7120dfc03b1321ca9fb6a8b912618
hemant-rawat-2002 <144592668+hemant-rawat-2002@users.noreply.github.com>
2025-08-23 13:52:41 +0530

    Add files via upload

---
+++ q1.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	
+	int first_number,second_number;
+	cout<<"Enter first number:";
+	cin>>first_number;
+	
+	cout<<"Enter second number:";
+	cin>>second_number;
+	
+	(first_number>second_number)?(cout<<" Greater number is : "<<first_number):(cout<<" Greater number is : "<<second_number);
+	//(condition)?true wali expression:false wali expression;
+	a
+	return 0;
+}
